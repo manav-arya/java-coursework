ddc02368ced00b3a5b8cf059d8b6ddb565f47930
Manav Arya <manav.arya24.18@gmail.com>
2026-06-15 15:25:55 +0530

    Introduction to StringBuilder and setCharAt function

---
+++ SBuilder.java
@@ -0,0 +1,11 @@
+public class SBuilder {
+    public static void main(String[] args) {
+        //Introduction
+        StringBuilder sb = new StringBuilder("Manav");// the advantage of string builder over strings is that string builder is mutable while strings create a new variable, change the pointer location and delete the old one 
+        System.out.println(sb);
+
+        //setCharAt
+        sb.setCharAt(-1, 'n');
+        System.out.println(sb);
+    }
+}
