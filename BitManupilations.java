2cef7e6ee2e9a547c68527f1ef05e51477dbdd2c
Manav Arya <manav.arya24.18@gmail.com>
2026-06-15 19:12:22 +0530

    Lecture 15, Bit Manupilation

---
+++ BitManupilations.java
@@ -0,0 +1,28 @@
+public class BitManupilations {
+    public static void main(String[] args) {
+        //Get Bit Operation
+        // Question from lecture
+        int n = 5;//0101
+        int pos = 1;
+        int bitMask = 1<<pos; //1000
+        if((bitMask& n)==0){
+            System.out.println("bit is 0");
+        }
+
+        // Set Bit
+        if((bitMask|n)==0){
+            System.out.println("bit is 0");
+        }else{
+            System.out.println("bit is one");
+        }
+
+        // Clear Bit
+        if((~bitMask&n)==0){
+            System.out.println("bit is 0");
+        }else{
+            System.out.println("bit is one");
+        }
+
+        //update bit ---> reffer to lecture
+    }
+}
